b403a7e7169d6b7ea72c8f091a9747417f535179
Geoffroy Vincent <83596426+kushuh@users.noreply.github.com>
2024-11-03 21:44:31 +0100

    feat: add various utils (#14)

--- testutils/grpc.go
+++ testutils/grpc.go
@@ -1,10 +1,14 @@
 package testutils
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/status"
 )
 
@@ -23,4 +27,22 @@ func RequireGRPCCodesEqual(t *testing.T, err error, code codes.Code) {
 	} else {
 		require.NoError(t, err)
 	}
+}
+
+// WaitConn expects a conn to enter ready state. It returns when conn is available, otherwise it fails the test.
+func WaitConn(t *testing.T, conn *grpc.ClientConn) {
+	t.Helper()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	healthClient := healthpb.NewHealthClient(conn)
+	require.Eventually(
+		t,
+		func() bool {
+			res, err := healthClient.Check(ctx, &healthpb.HealthCheckRequest{})
+			return err == nil && res.GetStatus() == healthpb.HealthCheckResponse_SERVING
+		},
+		10*time.Second, 100*time.Millisecond,
+	)
 }
