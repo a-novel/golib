d4eae2a6337f5fc5f97fbc0e2aca3f75bb8f7fe9
Geoffroy Vincent <83596426+kushuh@users.noreply.github.com>
2024-11-04 18:26:35 +0100

    style: add sugar syntax wrapper for grpc handlers (#16)

---
+++ grpc/handlers.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/a-novel/golib/loggers/adapters"
+)
+
+type ExecService[In any, Out any] interface {
+	Exec(ctx context.Context, data In) (Out, error)
+}
+
+type wrappedService[In any, Out any] struct {
+	service adapters.GrpcCallback[In, Out]
+}
+
+func (s *wrappedService[In, Out]) Exec(ctx context.Context, data In) (Out, error) {
+	return s.service(ctx, data)
+}
+
+func ServiceWithMetrics[In any, Out any](
+	name string, service ExecService[In, Out], logger adapters.GRPC,
+) ExecService[In, Out] {
+	return &wrappedService[In, Out]{
+		service: adapters.WrapGRPCCall(name, logger, service.Exec),
+	}
+}
