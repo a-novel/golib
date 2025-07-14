de7da335b200384b879915f84357832d92197a10
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-15 00:49:28 +0200

    fix: improve isolated postgres test stability

--- postgres/test_utils.go
+++ postgres/test_utils.go
@@ -18,10 +18,15 @@ import (
 
 type TransactionalTestFunc func(context.Context, *testing.T, *bun.DB)
 
-const CreateThrowawayDB = "CREATE DATABASE %s;"
+const CreateThrowawaySchema = "CREATE SCHEMA %s;"
 
 const NameLen = 31
 
+// RunIsolatedTransactionalTest runs test in a temporary throwaway schema. This allows for operations that cannot
+// be performed concurrently in a transactional context, such as refreshing materialized views.
+//
+// This method uses a separate schema, rather than a new database, so existing extensions are still available. It
+// still requires to rerun the whole migration process, so unless needed, RunTransactionalTest should be preferred.
 func RunIsolatedTransactionalTest(t *testing.T, config postgrespresets.DefaultConfig, callback TransactionalTestFunc) {
 	t.Helper()
 
@@ -31,16 +36,18 @@ func RunIsolatedTransactionalTest(t *testing.T, config postgrespresets.DefaultCo
 	require.NoError(t, WaitForDB(t.Context(), client))
 
 	// Create a new, temporary throwaway database.
-	dbName := "ta_" + strings.ToLower(rand.Text())
-	dbName = fmt.Sprintf("%.*s", NameLen, dbName)
+	schemaName := "ta_" + strings.ToLower(rand.Text())
+	schemaName = fmt.Sprintf("%.*s", NameLen, schemaName)
 
-	query := client.NewRaw(fmt.Sprintf(CreateThrowawayDB, dbName))
+	query := client.NewRaw(fmt.Sprintf(CreateThrowawaySchema, schemaName))
 	_, err = query.Exec(t.Context())
 	require.NoError(t, err, query.String())
 
 	sqldb := sql.OpenDB(pgdriver.NewConnector(
 		pgdriver.WithDSN(config.DSN),
-		pgdriver.WithDatabase(dbName),
+		pgdriver.WithConnParams(map[string]interface{}{
+			"search_path": schemaName,
+		}),
 	))
 	throwawayClient := bun.NewDB(sqldb, pgdialect.New(), bun.WithDiscardUnknownColumns())
 
@@ -57,6 +64,9 @@ func RunIsolatedTransactionalTest(t *testing.T, config postgrespresets.DefaultCo
 	callback(ctxPG, t, throwawayClient)
 }
 
+// RunTransactionalTest creates a special transactional context for testing. This context uses the PassthroughTx
+// implementation, that allows for concurrent tests with the same database connection. It discards sub-transactions
+// to prevent deadlocks.
 func RunTransactionalTest(t *testing.T, db *bun.DB, callback TransactionalTestFunc) {
 	t.Helper()
 
