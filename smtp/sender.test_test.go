42762a56d266569f2b82df627c74bbcd925a6192
Kushuh <geoffroy.vincent@agoradesecrivains.com>
2025-07-16 19:09:29 +0200

    fix: improve test mail stability

--- smtp/sender.test_test.go
+++ smtp/sender.test_test.go
@@ -14,48 +14,18 @@ func TestTestSender(t *testing.T) {
 
 	sender := smtp.NewTestSender()
 
-	listenCondition1 := func(mail *smtp.TestMail) bool {
-		mapData, ok := mail.Data.(map[string]any)
-		if !ok {
-			return false
-		}
+	require.NoError(t, sender.SendMail([]string{"user"}, nil, "", map[string]string{"test": "foo"}))
+	require.NoError(t, sender.SendMail([]string{"user"}, nil, "", map[string]string{"test": "bar"}))
 
-		return mapData["test"] == "foo"
-	}
-
-	listenCondition2 := func(mail *smtp.TestMail) bool {
-		mapData, ok := mail.Data.(map[string]any)
-		if !ok {
-			return false
-		}
-
-		return mapData["test"] == "bar"
-	}
-
-	listenCondition3 := func(mail *smtp.TestMail) bool {
-		mapData, ok := mail.Data.(map[string]any)
-		if !ok {
-			return false
-		}
-
-		return mapData["test"] == "baz"
-	}
-
-	waiter1 := sender.GetWaiter(listenCondition1, time.Second)
-	waiter2 := sender.GetWaiter(listenCondition2, time.Second)
-	waiter3 := sender.GetWaiter(listenCondition3, time.Second)
-
-	go func() {
-		_ = sender.SendMail([]string{"user"}, nil, "", map[string]any{"test": "foo"})
-	}()
-
-	go func() {
-		_ = sender.SendMail([]string{"user"}, nil, "", map[string]any{"test": "bar"})
-	}()
-
-	res1, ok1 := waiter1.Wait()
-	res2, ok2 := waiter2.Wait()
-	res3, ok3 := waiter3.Wait()
+	res1, ok1 := sender.FindTestMail(func(mail *smtp.TestMail) bool {
+		return mail.Data.(map[string]string)["test"] == "foo"
+	}, time.Second)
+	res2, ok2 := sender.FindTestMail(func(mail *smtp.TestMail) bool {
+		return mail.Data.(map[string]string)["test"] == "bar"
+	}, time.Second)
+	res3, ok3 := sender.FindTestMail(func(mail *smtp.TestMail) bool {
+		return mail.Data.(map[string]string)["test"] == "baz"
+	}, time.Second)
 
 	require.True(t, ok1)
 	require.True(t, ok2)
@@ -63,11 +33,11 @@ func TestTestSender(t *testing.T) {
 
 	require.Equal(t, &smtp.TestMail{
 		To:   []string{"user"},
-		Data: map[string]any{"test": "foo"},
+		Data: map[string]string{"test": "foo"},
 	}, res1)
 	require.Equal(t, &smtp.TestMail{
 		To:   []string{"user"},
-		Data: map[string]any{"test": "bar"},
+		Data: map[string]string{"test": "bar"},
 	}, res2)
 	require.Nil(t, res3)
 }
