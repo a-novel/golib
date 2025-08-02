aa0b555efc22d9e76096ffa81be7363b7fe5303d
Kushuh <kuzanisu@gmail.com>
2025-08-02 14:44:05 +0200

    fix(renovate): post upgrade action

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Install node on the renovate image.
-curl -o- https://fnm.vercel.app/install | bash
-eval "$(fnm env)"
-fnm install 24
+curl -o- https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.fnm"
+eval "$("$HOME/.fnm" env)"
+"$HOME/.fnm" install 24
 
 node -v || echo "node install failed" && exit 1
 npm -v || echo "npm install failed" && exit 1
