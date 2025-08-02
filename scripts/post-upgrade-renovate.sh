a252d2b797977fa14ea99cedeaf6cf0c37fbce09
Kushuh <kuzanisu@gmail.com>
2025-08-02 15:26:45 +0200

    fix(renovate): post upgrade action

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -9,7 +9,7 @@ FNM="$FNM_DIR/fnm"
 
 curl -o- https://fnm.vercel.app/install | bash -s -- --install-dir "$FNM_DIR"
 eval "$("$FNM" env)"
-"$FNM" install --latest --log-level error --use
+"$FNM" install --latest --log-level error
 
 which node || echo "node install failed" && exit 1
 which npm || echo "npm install failed" && exit 1
