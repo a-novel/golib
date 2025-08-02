cb98bc2b093ea60d316799999c5de4078b36af84
Kushuh <kuzanisu@gmail.com>
2025-08-02 15:35:15 +0200

    fix(renovate): post upgrade action

--- scripts/post-upgrade-renovate.sh
+++ scripts/post-upgrade-renovate.sh
@@ -2,18 +2,13 @@
 
 set -e
 
-# ======================================================================================================================
-# Install node on the renovate image.
-FNM_DIR="$HOME/.fnm"
-FNM="$FNM_DIR/fnm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
 
-curl -o- https://fnm.vercel.app/install | bash -s -- --install-dir "$FNM_DIR"
-eval "$("$FNM" env --log-level error --fnm-dir "$FNM_DIR")"
-"$FNM" install --latest --log-level error
+nvm install node
 
-which node || echo "node install failed" && exit 1
-which npm || echo "npm install failed" && exit 1
-which npx || echo "npx install failed" && exit 1
-# ======================================================================================================================
+ env | grep NVM
+ls -al ./scripts
+exit 1
 
 npx -y prettier . --write
