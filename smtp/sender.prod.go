312291ae58a825279650d42acd3c1117f2fad150
Geoffroy Vincent <geoffroy.vincent@agorastoryverse.com>
2025-11-09 16:09:46 +0100

    feat(smtp): allow local unsecure connections (#70)

--- smtp/sender.prod.go
+++ smtp/sender.prod.go
@@ -14,6 +14,9 @@ type ProdSender struct {
 	Email    string `json:"email"    yaml:"email"`
 	Password string `json:"password" yaml:"password"`
 	Domain   string `json:"domain"   yaml:"domain"`
+
+	// Dangerous setting, only use for local development.
+	ForceUnencryptedTls bool `json:"forceUnencryptedTLS" yaml:"forceUnencryptedTLS"`
 }
 
 func (sender *ProdSender) SendMail(to MailUsers, t *template.Template, tName string, data any) error {
@@ -25,6 +28,9 @@ func (sender *ProdSender) SendMail(to MailUsers, t *template.Template, tName str
 	}
 
 	auth := smtp.PlainAuth(sender.Name, sender.Email, sender.Password, sender.Domain)
+	if sender.ForceUnencryptedTls {
+		auth = unencryptedAuth{auth}
+	}
 
 	msg := fmt.Sprintf("From: %s <%s>\r\n", sender.Name, sender.Email)
 	msg += fmt.Sprintf("To: %s\r\n", to.String())
@@ -40,6 +46,9 @@ func (sender *ProdSender) SendMail(to MailUsers, t *template.Template, tName str
 
 func (sender *ProdSender) Ping() error {
 	auth := smtp.PlainAuth(sender.Name, sender.Email, sender.Password, sender.Domain)
+	if sender.ForceUnencryptedTls {
+		auth = unencryptedAuth{auth}
+	}
 
 	conn, err := smtp.Dial(sender.Addr)
 	if err != nil {
@@ -57,4 +66,15 @@ func (sender *ProdSender) Ping() error {
 	}
 
 	return nil
+}
+
+type unencryptedAuth struct {
+	smtp.Auth
+}
+
+func (a unencryptedAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	s := *server
+	s.TLS = true
+
+	return a.Auth.Start(&s)
 }
