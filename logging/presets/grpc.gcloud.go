e68cd04a560bbf69ddf6f069203cdf499ae59452
Geoffroy Vincent <geoffroy.vincent@agorastoryverse.com>
2025-11-13 22:33:38 +0100

    feat(logging): add panic interceptors (#79)

--- logging/presets/grpc.gcloud.go
+++ logging/presets/grpc.gcloud.go
@@ -1,14 +1,12 @@
 package loggingpresets
 
 import (
-	"context"
 	"log/slog"
 	"os"
 
 	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"google.golang.org/grpc"
-
-	"go.opentelemetry.io/otel/trace"
 
 	"github.com/a-novel/golib/logging"
 )
@@ -24,13 +22,25 @@ type GrpcGcloud struct {
 func (logger *GrpcGcloud) UnaryInterceptor() grpc.UnaryServerInterceptor {
 	logger.init()
 
-	return grpclog.UnaryServerInterceptor(gcloudInterceptor(logger.l), grpclog.WithFieldsFromContext(gcloudLogTraceId))
+	return grpclog.UnaryServerInterceptor(logInterceptor(logger.l), grpclog.WithFieldsFromContext(logTraceId))
 }
 
 func (logger *GrpcGcloud) StreamInterceptor() grpc.StreamServerInterceptor {
 	logger.init()
 
-	return grpclog.StreamServerInterceptor(gcloudInterceptor(logger.l), grpclog.WithFieldsFromContext(gcloudLogTraceId))
+	return grpclog.StreamServerInterceptor(logInterceptor(logger.l), grpclog.WithFieldsFromContext(logTraceId))
+}
+
+func (logger *GrpcGcloud) PanicUnaryInterceptor() grpc.UnaryServerInterceptor {
+	logger.init()
+
+	return recovery.UnaryServerInterceptor(recovery.WithRecoveryHandler(panicInterceptor(logger.l)))
+}
+
+func (logger *GrpcGcloud) PanicStreamInterceptor() grpc.StreamServerInterceptor {
+	logger.init()
+
+	return recovery.StreamServerInterceptor(recovery.WithRecoveryHandler(panicInterceptor(logger.l)))
 }
 
 func (logger *GrpcGcloud) init() {
@@ -40,18 +50,4 @@ func (logger *GrpcGcloud) init() {
 
 	log := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{}))
 	logger.l = log.With("service", "gRPC/server", "component", logger.Component)
-}
-
-func gcloudInterceptor(l *slog.Logger) grpclog.Logger {
-	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, fields ...any) {
-		l.Log(ctx, slog.Level(lvl), msg, fields...)
-	})
-}
-
-func gcloudLogTraceId(ctx context.Context) grpclog.Fields {
-	if span := trace.SpanContextFromContext(ctx); span.IsSampled() {
-		return grpclog.Fields{"traceID", span.TraceID().String()}
-	}
-
-	return nil
 }
--- logging/presets/grpc.local.go
+++ logging/presets/grpc.local.go
@@ -1,14 +1,12 @@
 package loggingpresets
 
 import (
-	"context"
 	"log/slog"
 
 	"github.com/fatih/color"
 	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"google.golang.org/grpc"
-
-	"go.opentelemetry.io/otel/trace"
 
 	"github.com/a-novel/golib/logging"
 )
@@ -24,13 +22,25 @@ type GrpcLocal struct {
 func (logger *GrpcLocal) UnaryInterceptor() grpc.UnaryServerInterceptor {
 	logger.init()
 
-	return grpclog.UnaryServerInterceptor(localInterceptor(logger.l), grpclog.WithFieldsFromContext(localILogTraceId))
+	return grpclog.UnaryServerInterceptor(logInterceptor(logger.l), grpclog.WithFieldsFromContext(logTraceId))
 }
 
 func (logger *GrpcLocal) StreamInterceptor() grpc.StreamServerInterceptor {
 	logger.init()
 
-	return grpclog.StreamServerInterceptor(localInterceptor(logger.l), grpclog.WithFieldsFromContext(localILogTraceId))
+	return grpclog.StreamServerInterceptor(logInterceptor(logger.l), grpclog.WithFieldsFromContext(logTraceId))
+}
+
+func (logger *GrpcLocal) PanicUnaryInterceptor() grpc.UnaryServerInterceptor {
+	logger.init()
+
+	return recovery.UnaryServerInterceptor(recovery.WithRecoveryHandler(panicInterceptor(logger.l)))
+}
+
+func (logger *GrpcLocal) PanicStreamInterceptor() grpc.StreamServerInterceptor {
+	logger.init()
+
+	return recovery.StreamServerInterceptor(recovery.WithRecoveryHandler(panicInterceptor(logger.l)))
 }
 
 func (logger *GrpcLocal) init() {
@@ -40,18 +50,4 @@ func (logger *GrpcLocal) init() {
 
 	log := slog.New(slog.NewTextHandler(color.Output, &slog.HandlerOptions{}))
 	logger.l = log.With("service", "gRPC/server", "component", logger.Component)
-}
-
-func localInterceptor(l *slog.Logger) grpclog.Logger {
-	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, fields ...any) {
-		l.Log(ctx, slog.Level(lvl), msg, fields...)
-	})
-}
-
-func localILogTraceId(ctx context.Context) grpclog.Fields {
-	if span := trace.SpanContextFromContext(ctx); span.IsSampled() {
-		return grpclog.Fields{"traceID", span.TraceID().String()}
-	}
-
-	return nil
 }
