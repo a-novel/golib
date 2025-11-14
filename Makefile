4db04f2da0d6ea3796df1f0c531b1c5b61cee62e
Geoffroy Vincent <geoffroy.vincent@agorastoryverse.com>
2025-11-14 14:42:12 +0100

    feat(grpc): add unary echo server (#81)

--- Makefile
+++ Makefile
@@ -5,10 +5,19 @@ test:
 # Check code quality.
 lint:
 	go tool golangci-lint run
+	go tool buf lint
 	pnpm lint
+
+# Generate Go code.
+generate-go:
+	go generate ./...
+
+generate: generate-go
 
 # Reformat code so it passes the code style lint checks.
 format:
 	go mod tidy
 	go tool golangci-lint run --fix
+	go tool buf format -w
+	go tool buf dep update
 	pnpm format
---
+++ grpcf/echo.go
@@ -0,0 +1,46 @@
+package grpcf
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+
+	golibproto "github.com/a-novel/golib/grpcf/proto/gen"
+)
+
+type echo struct {
+	golibproto.UnimplementedEchoServiceServer
+}
+
+func (handler *echo) UnaryEcho(context.Context, *golibproto.UnaryEchoRequest) (*golibproto.UnaryEchoResponse, error) {
+	return &golibproto.UnaryEchoResponse{
+		Message: "Hello world!",
+	}, nil
+}
+
+func SetEchoServers(server *grpc.Server, healthPing time.Duration) {
+	healthcheck := health.NewServer()
+	healthpb.RegisterHealthServer(server, healthcheck)
+
+	golibproto.RegisterEchoServiceServer(server, new(echo))
+
+	go func() {
+		// asynchronously inspect dependencies and toggle serving status as needed
+		next := healthpb.HealthCheckResponse_SERVING
+
+		for {
+			healthcheck.SetServingStatus("", next)
+
+			if next == healthpb.HealthCheckResponse_SERVING {
+				next = healthpb.HealthCheckResponse_NOT_SERVING
+			} else {
+				next = healthpb.HealthCheckResponse_SERVING
+			}
+
+			time.Sleep(healthPing)
+		}
+	}()
+}
